62d57acb18cbb5a7129be8155a6dd8632fe6cf1f
Johan <johan.bertrand@le-campus-numerique.fr>
2025-01-17 10:48:42 +0100

    rebase clientAccount:landingPage

---
+++ components/userAccount/clientAccount.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Client Account</h1>
+  </div>
+  <div>
+    <input type="text" v-model="clientName" placeholder="Client Name" />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  vite: {
+      resolve: {
+          alias: {
+              '#app-manifest': '/home/bertrand/Bureau/hotel_artichoke_vue/node_modules/nuxt/dist/app/composables/manifest.js'
+          }
+      }
+  },
 
-})
+  compatibilityDate: '2025-01-17'
+});
