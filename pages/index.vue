51f339dbe8fe4c0e833e5477db9bc5cf1762e422
claire <claire.deheegher@le-campus-numerique.fr>
2025-01-17 11:16:28 +0100

    Resolved parse bug on components by changing names and deleting scripts balises

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="page bg-amber-50">
     <Header/>
     <LandingPageHeroe/>
@@ -8,9 +9,3 @@
   </div>
 
 </template>
-<script setup lang="ts">
-import LandingPageHeroe from "~/components/landingPage/LandingPageHeroe.vue";
-import LandingPageAmenities from "~/components/landingPage/LandingPageAmenities.vue";
-import LandingPageRooms from "~/components/landingPage/LandingPageRooms.vue";
-import LandingPageNews from "~/components/landingPage/LandingPageNews.vue";
-</script>
