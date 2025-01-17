0dc2948bc9eb74b59c6deca07fbc84ed48970842
Johan <johan.bertrand@le-campus-numerique.fr>
2025-01-17 12:32:23 +0100

    login ok for mvp

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-
   <div class="page bg-amber-50">
-    <Header/>
-    <LandingPageHeroe/>
-    <LandingPageAmenities/>
-    <LandingPageRooms/>
-    <LandingPageNews/>
-  </div>
+    <Header />
+    <LandingPageHeroe />
+    <LandingPageAmenities />
+    <LandingPageRooms />
+    <LandingPageNews />
 
+    <!-- Button to redirect to clientAccount page -->
+    <router-link to="/login">
+      <button class="btn btn-primary">Login</button>
+    </router-link>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Header from '~/components/Header.vue'
+import LandingPageHeroe from '~/components/landingPage/Heroe.vue'
+import LandingPageAmenities from '~/components/landingPage/Amenities.vue'
+import LandingPageRooms from '~/components/landingPage/Rooms.vue'
+import LandingPageNews from '~/components/landingPage/News.vue'
+</script>
