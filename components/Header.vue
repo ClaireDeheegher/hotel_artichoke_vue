5835db12d9e34bf4ab1c06d9931aba027f6b3ade
claire <claire.deheegher@le-campus-numerique.fr>
2025-01-17 11:30:51 +0100

    Made basic header to show it, need to be modified for website purposes

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,33 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark ">
-    <div class="container"></div>
-  </nav>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+nav ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+}
+nav a {
+  color: white;
+  text-decoration: none;
+}
+</style>
