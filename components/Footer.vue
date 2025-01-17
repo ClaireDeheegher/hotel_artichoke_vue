f7a6ff221bab5a1617f16decb54eac396ebe8e04
Johan <johan.bertrand@le-campus-numerique.fr>
2025-01-17 10:48:42 +0100

    rebase clientAccount:landingPage

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Contenu de votre composant -->
+  </div>
+</template>
+
+<script setup>
+// Logique de votre composant
+</script>
+
+<style scoped>
+/* Styles de votre composant */
+</style>
