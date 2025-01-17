64fe8d480f7442811834cb4b487f5266f20a49b8
claire <claire.deheegher@le-campus-numerique.fr>
2025-01-17 09:33:19 +0100

    Created landingPage folder and refactor components names for merging purposes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="page bg-amber-50">
     <Header/>
-    <Heroe/>
+    <LandingPageHeroe/>
+    <LandingPageAmenities/>
+    <LandingPageRooms/>
+    <LandingPageNews/>
   </div>
 
 </template>
 <script setup lang="ts">
+import LandingPageHeroe from "~/components/landingPage/LandingPageHeroe.vue";
+import LandingPageAmenities from "~/components/landingPage/LandingPageAmenities.vue";
+import LandingPageRooms from "~/components/landingPage/LandingPageRooms.vue";
+import LandingPageNews from "~/components/landingPage/LandingPageNews.vue";
 </script>
