539627f8ac5519aa2c17bb5de838af3588da4831
Johan <johan.bertrand@le-campus-numerique.fr>
2025-01-17 11:30:57 +0100

    clientAccount wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 export default defineNuxtConfig({
-  vite: {
-      resolve: {
-          alias: {
-              '#app-manifest': '/home/bertrand/Bureau/hotel_artichoke_vue/node_modules/nuxt/dist/app/composables/manifest.js'
-          }
-      }
-  },
+    // Configuration pour Vite
+    vite: {
+        resolve: {
+            alias: {
+                '#app-manifest': '/home/bertrand/Bureau/hotel_artichoke_vue/node_modules/nuxt/dist/app/composables/manifest.js'
+            }
+        }
+    },
 
-  compatibilityDate: '2025-01-17'
-});
+    // Ajouter le module Tailwind CSS
+    modules: ['@nuxtjs/tailwindcss'],
+
+    // Configuration de compatibilité (facultatif)
+    compatibilityDate: '2025-01-17',
+})
